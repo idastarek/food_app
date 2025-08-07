77a599b0179e8cd485d2c855a8ff3e4c3b147802
Ida <ida.starek@gmail.com>
2025-08-07 17:53:19 +0100

    move tests to components folder, changed button text name

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -3,24 +3,24 @@ import userEvent from '@testing-library/user-event';
 import Button from '../components/Button';
 
 describe(Button, () => {
-    const buttonText = "Get recipes!";
+    const BUTTON_TEXT = "Get recipes!";
     
     it('renders with correct text', () => {
-        render(<Button type="button" text={buttonText} />);
-        const button = screen.getByText(buttonText);
+        render(<Button type="button" text={BUTTON_TEXT} />);
+        const button = screen.getByText(BUTTON_TEXT);
         expect(button).toBeInTheDocument();
     })  
   
     it('calls onClick handler when clicked', async() => {
         const handleClick = vi.fn();
-        render(<Button type="button" text={buttonText} onClick={handleClick} />);
-        await userEvent.click(screen.getByText(buttonText));
+        render(<Button type="button" text={BUTTON_TEXT} onClick={handleClick} />);
+        await userEvent.click(screen.getByText(BUTTON_TEXT));
         expect (handleClick).toHaveBeenCalled();
     })
 
     it('has the correct type attribute', () => {
-        render(<Button type="button" text={buttonText}/>);
-        const button = screen.getByText(buttonText);
+        render(<Button type="button" text={BUTTON_TEXT}/>);
+        const button = screen.getByText(BUTTON_TEXT);
         expect(button).toHaveAttribute('type', 'button');
     })
 })
