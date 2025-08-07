4314b00affa4e42980d597d7720e9fe623d6b247
Ida <ida.starek@gmail.com>
2025-08-07 19:22:37 +0100

    changed variable name, moved test to components folder, updated dependencies

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,26 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import Button from '../components/Button';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Button from "../components/Button";
 
 describe(Button, () => {
-    const BUTTON_TEXT = "Get recipes!";
-    
-    it('renders with correct text', () => {
-        render(<Button type="button" text={BUTTON_TEXT} />);
-        const button = screen.getByText(BUTTON_TEXT);
-        expect(button).toBeInTheDocument();
-    })  
-  
-    it('calls onClick handler when clicked', async() => {
-        const handleClick = vi.fn();
-        render(<Button type="button" text={BUTTON_TEXT} onClick={handleClick} />);
-        await userEvent.click(screen.getByText(BUTTON_TEXT));
-        expect (handleClick).toHaveBeenCalled();
-    })
+  const BUTTON_TEXT = "Get recipes!";
 
-    it('has the correct type attribute', () => {
-        render(<Button type="button" text={BUTTON_TEXT}/>);
-        const button = screen.getByText(BUTTON_TEXT);
-        expect(button).toHaveAttribute('type', 'button');
-    })
-})
+  it("renders with correct text", () => {
+    render(<Button type="button" text={BUTTON_TEXT} />);
+    const button = screen.getByText(BUTTON_TEXT);
+    expect(button).toBeInTheDocument();
+  });
+
+  it("calls onClick handler when clicked", async () => {
+    const handleClick = vi.fn();
+    render(<Button type="button" text={BUTTON_TEXT} onClick={handleClick} />);
+    await userEvent.click(screen.getByText(BUTTON_TEXT));
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it("has the correct type attribute", () => {
+    render(<Button type="button" text={BUTTON_TEXT} />);
+    const button = screen.getByText(BUTTON_TEXT);
+    expect(button).toHaveAttribute("type", "button");
+  });
+});
