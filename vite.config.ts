67a753f2e354ac6d76291025bdb3bc3c67b7fb6a
Ida Starek <ida.starek@gmail.com>
2025-08-12 19:50:31 +0100

    set up tailwind css - downgraded to v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 
-// /// <reference types="vitest" />
-// /// <reference types="vite/client" />
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-     //@ts-ignore
-    tailwindcss()
   ],
   test: {
     globals: true,
