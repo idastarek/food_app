1d006894bca9ac4445db6b1b85c9e1b253a7201d
mongru <monika.grubizna@gmail.com>
2025-08-22 14:59:44 +0100

    fix form and writing to the db

--- frontend/src/components/Ingredient.tsx
+++ frontend/src/components/Ingredient.tsx
@@ -1,5 +1,4 @@
-// ingredient interface
-export interface IngredientType  {
+export interface IngredientType {
   ingredient_name: string;
   ingredient_quantity: string;
   ingredient_unit: string;
@@ -8,25 +7,37 @@ export interface IngredientType  {
 }
 
 // ingredient component
-export default function Ingredient({ ingredient_name, ingredient_quantity, ingredient_unit, imageUrl, onDelete }: IngredientType) {
+export default function Ingredient({
+  ingredient_name,
+  ingredient_quantity,
+  ingredient_unit,
+  imageUrl,
+  onDelete,
+}: IngredientType) {
   return (
     <>
-      <div className="ingredient-container">
+      <div className="ingredient-container" key={ingredient_name}>
         <h3 className="ingredient-name">{ingredient_name}</h3>
-        <img src={imageUrl} alt={`icon of ${ingredient_name}`} className="ingredient-icon" />
+        <img
+          src={imageUrl}
+          alt={`icon of ${ingredient_name}`}
+          className="ingredient-icon"
+        />
         <button
           onClick={onDelete}
-          className="remove-ingredient-btn" 
+          className="remove-ingredient-btn"
           aria-label={`Remove ${ingredient_name}`}
         >
-          <img 
-            src="/images/x-button.png" 
-            alt="remove ingredient" 
+          <img
+            src="/images/x-button.png"
+            alt="remove ingredient"
             className="h-5 w-5"
           />
         </button>
-        <p className="ingredient-quantity">{ingredient_quantity} {ingredient_unit}</p>
+        <p className="ingredient-quantity">
+          {ingredient_quantity} {ingredient_unit}
+        </p>
       </div>
     </>
-  )
+  );
 }
