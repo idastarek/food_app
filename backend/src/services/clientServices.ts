2c1392013512ab30b383da84c3807ab6bb82825f
Ida <ida.starek@gmail.com>
2025-09-09 19:13:51 +0100

    merged recent changes from main

--- backend/src/services/clientServices.ts
+++ backend/src/services/clientServices.ts
@@ -1,8 +1,8 @@
 import { query } from '../db';
 interface IngredientType {
-  ingredient_name: string;
-  ingredient_quantity: string;
-  ingredient_unit: string;
+  ingredientName: string;
+  ingredientQuantity: string;
+  ingredientUnit: string;
   imageUrl?: string;
 }
 
@@ -12,9 +12,9 @@ export const getAllIngredients = async () => {
 };
 
 export const createIngredient = async (ingredientData: IngredientType) => {
-    const {ingredient_name, ingredient_quantity, ingredient_unit} = ingredientData;
+    const {ingredientName: ingredientName, ingredientQuantity: ingredientQuantity, ingredientUnit: ingredientUnit} = ingredientData;
     const { rows } = await query(`
-        INSERT INTO ingredients (ingredient_name, ingredient_quantity, ingredient_unit) 
-        VALUES ($1, $2, $3) RETURNING *`, [ingredient_name, ingredient_quantity, ingredient_unit]);
+        INSERT INTO ingredients (ingredientName, ingredientQuantity, ingredientUnit) 
+        VALUES ($1, $2, $3) RETURNING *`, [ingredientName, ingredientQuantity, ingredientUnit]);
     return rows[0];
 }
