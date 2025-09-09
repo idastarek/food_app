4107c80fc3712b817fb215c92b6a99d1b65a2cc8
Ida <ida.starek@gmail.com>
2025-09-09 18:55:51 +0100

    changed paths to be consistent

--- frontend/src/ingredientIcons.ts
+++ frontend/src/ingredientIcons.ts
@@ -6,8 +6,8 @@ export interface IngredientIconsType{
 export const ingredientIcons: IngredientIconsType = {
   tomato: "/icons/tomato.png", 
   bread: "/icons/bread.png",
-  potato: "icons/potato.png",
-  carrot: "icons/carrot.png",
-  yoghurt: "icons/yoghurt.png",
+  potato: "/icons/potato.png",
+  carrot: "/icons/carrot.png",
+  yoghurt: "/icons/yoghurt.png",
   default: "/icons/potato.png",
 };
