f1770a48da83306f4482e63eeb8b385866d67463
Ida <ida.starek@gmail.com>
2025-09-22 07:09:37 +0100

    connect frontend-backend to fetch ingredients from db

--- frontend/src/pages/AddIngredients.tsx
+++ frontend/src/pages/AddIngredients.tsx
@@ -1,80 +1,69 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../styles/AddIngredients.scss';
-import Title from '../components/Title.tsx';
-import InputField from '../components/InputField.tsx';
-import SelectDropdown from '../components/SelectDropdown.tsx';
-import Button from '../components/Button.tsx';
-import ItemsGrid from '../components/ItemsGrid.tsx';
-import Ingredient from '../components/Ingredient.tsx';
-import type { IngredientType}  from '../components/Ingredient.tsx';
-import ingredients from '../../src/data/ingredients.json';
-import { hasEmptyFields } from '../utils/formValidation.ts'
-import { ingredientIcons } from '../ingredientIcons.ts';
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../styles/AddIngredients.scss";
+import Title from "../components/Title.tsx";
+import InputField from "../components/InputField.tsx";
+import SelectDropdown from "../components/SelectDropdown.tsx";
+import Button from "../components/Button.tsx";
+import ItemsGrid from "../components/ItemsGrid.tsx";
+import Ingredient from "../components/Ingredient.tsx";
+import type { IngredientType } from "../components/Ingredient.tsx";
+import { hasEmptyFields } from "../utils/formValidation.ts";
+import { ingredientIcons } from "../ingredientIcons.ts";
 
 function GetRecipesButton() {
   const navigate = useNavigate();
   function handleRedirect() {
-    navigate('/recipe-suggestions');
+    navigate("/recipe-suggestions");
   }
 
   return (
-        <div id="get-recipes-btn">
-            <Button 
-              type="button"
-              text="Get recipes!"
-              onClick={handleRedirect}
-            />
-        </div>
+    <div id="get-recipes-btn">
+      <Button type="button" text="Get recipes!" onClick={handleRedirect} />
+    </div>
   );
 }
 
+export default function AddIngredients() {
 
-export default function AddIngredients() {  
-  
+  //TODO: delete this 
+
   // set the default ingredients values to the ingredients from the json file
-  const defaultIngredientsValues: IngredientType[] = ingredients;
-  console.log("default ingredients values", defaultIngredientsValues);
+  // const defaultIngredientsValues: IngredientType[] = ingredients;
+  // console.log("default ingredients values", defaultIngredientsValues);
 
   // initialise an array to store ingredient objects
-  const [ingredientsArray, setIngredientsArray] = useState<IngredientType[]>(defaultIngredientsValues);
+  const [ingredientsArray, setIngredientsArray] = useState<IngredientType[]>([]);
 
   // empty input fields - resetting
   const resetInputData: IngredientType = {
     name: "",
     quantity: "",
     unit: "",
-    imageUrl: ""
-  }
+    imageUrl: "",
+  };
 
-  // adding ingredients from JSON on page load
+  // displaying ingredients user currently has from the database
   useEffect(() => {
-
-    try {
-      // check localStorage inside useEffect to get an up-to-date value
-      const existingIngredients = localStorage.getItem("ingredients");
-
-      if (!existingIngredients) {
-        if (ingredients.length) {
-          localStorage.setItem("ingredients", JSON.stringify(ingredients));
-          setIngredientsArray(ingredients);
-        }
-      } else {
-        const parsedIngredients = JSON.parse(existingIngredients);
-        setIngredientsArray(parsedIngredients);
-      } 
-    } catch (error) {
-        console.error("Error with localStorage:", error);
-        setIngredientsArray(ingredients);
-    }
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('http://localhost:3000/api/ingredients');
+          setIngredientsArray(response.data);
+      } catch (error) {
+        console.error('Error fetching ingredients', error);
+      }
+      };
+      fetchData();
   }, []);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value} = event.target;
-    setInputData((prev) => ({ 
-      ...prev, 
-      [name]: value 
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setInputData((prev) => ({
+      ...prev,
+      [name]: value,
     }));
   };
 
@@ -92,11 +81,10 @@ export default function AddIngredients() {
 
     console.log("ingredient", ingredient);
 
-
     // TODO add an error state and message for each input
 
     // validate if none of the fields was left empty
-    if (hasEmptyFields(ingredient, ['name', 'quantity', 'unit'])) {
+    if (hasEmptyFields(ingredient, ["name", "quantity", "unit"])) {
       alert("At least one of the fields is empty!");
       return;
     }
@@ -111,95 +99,98 @@ export default function AddIngredients() {
 
     // reset the input fields after data is submitted
     setInputData(resetInputData);
-  }
+  };
 
-  // TODO: 
+  // TODO:
   // (1) if user adds ingredient already present, combine the amounts rather than having
   // two separate ingredients
   // (2) "are you sure you want to delete?" notification
   // (3) long pressing / double clicking ingredient allows the user to readjust the quantity / delete
 
-
   const handleDeleteIngredient = (ingToDelete: string) => {
-    const updatedArray = ingredientsArray.filter(ingredient => ingredient.name !== ingToDelete);
+    const updatedArray = ingredientsArray.filter(
+      (ingredient) => ingredient.name !== ingToDelete
+    );
     setIngredientsArray(updatedArray);
     localStorage.setItem("ingredients", JSON.stringify(updatedArray));
-  }
+  };
 
   return (
     <>
-        <div id="container">
-            <div id="top-screen">
-                <Title title="I currently have..." />
-                <div id="input-fields">
-                  <form onSubmit={handleSubmit}>
-                    <InputField 
-                        as="input"
-                        type="text"
-                        inputItem="ingredient"
-                        name="name"
-                        placeholder="Log your food here"
-                        value={inputData.name}
-                        onChange={handleChange}
-                    />
+      <div id="container">
+        <div id="top-screen">
+          <Title title="I currently have..." />
+          <div id="input-fields">
+            <form onSubmit={handleSubmit}>
+              <InputField
+                as="input"
+                type="text"
+                inputItem="ingredient"
+                name="name"
+                placeholder="Log your food here"
+                value={inputData.name}
+                onChange={handleChange}
+              />
 
-                    <div id="form-quantity-fields">
-                      <InputField 
-                          as="input"
-                          type="text"
-                          inputItem="quantity"
-                          name="quantity"
-                          placeholder="0"
-                          value={inputData.quantity}
-                          onChange={handleChange}
-                      />
+              <div id="form-quantity-fields">
+                <InputField
+                  as="input"
+                  type="text"
+                  inputItem="quantity"
+                  name="quantity"
+                  placeholder="0"
+                  value={inputData.quantity}
+                  onChange={handleChange}
+                />
 
-                      <SelectDropdown 
-                        options={[
-                          { value: "", label: "Select unit" },
-                          { value: "g", label: "grams" },
-                          { value: "kg", label: "kilograms" },
-                          { value: "ml", label: "milliliters" },
-                          { value: "l", label: "liters" },
-                          { value: "pcs", label: "pieces" },
-                        ]}
-                        inputItem="unit"
-                        name="unit"
-                        placeholder="Select unit"
-                        value={inputData.unit}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <Button
-                      type="submit"
-                      text="Add ingredient"
-                      className="input-field-button"
-                    />
-                  </form>
-                </div>           
-            </div>
-            <div className="ingredients-container-outer">
-              <div className="ingredients-container-inner">
-                <ItemsGrid 
-                  itemsArray={ingredientsArray} 
-                  renderItem={(item, index) => {
-                    const imageUrl = ingredientIcons[item.name.toLowerCase()] || ingredientIcons.default
-                    return (
-                      <Ingredient
-                        key={item.name + index}
-                        name={item.name}
-                        quantity={item.quantity}
-                        unit={item.unit}
-                        imageUrl={imageUrl}
-                        onDelete={() => handleDeleteIngredient(item.name)}
-                      />
-                  )}
-                }
+                <SelectDropdown
+                  options={[
+                    { value: "", label: "Select unit" },
+                    { value: "g", label: "grams" },
+                    { value: "kg", label: "kilograms" },
+                    { value: "ml", label: "milliliters" },
+                    { value: "l", label: "liters" },
+                    { value: "pcs", label: "pieces" },
+                  ]}
+                  inputItem="unit"
+                  name="unit"
+                  placeholder="Select unit"
+                  value={inputData.unit}
+                  onChange={handleChange}
                 />
               </div>
-            </div>
-            {(ingredientsArray.length > 0) && <GetRecipesButton/>}
+              <Button
+                type="submit"
+                text="Add ingredient"
+                className="input-field-button"
+              />
+            </form>
+          </div>
         </div>
+        <div className="ingredients-container-outer">
+          <div className="ingredients-container-inner">
+            <ItemsGrid
+              itemsArray={ingredientsArray}
+              renderItem={(item, index) => {
+                const imageUrl =
+                  ingredientIcons[item.name.toLowerCase()] ||
+                  ingredientIcons.default;
+                return (
+                  <Ingredient
+                    key={item.name + index}
+                    name={item.name}
+                    quantity={item.quantity}
+                    unit={item.unit}
+                    imageUrl={imageUrl}
+                    onDelete={() => handleDeleteIngredient(item.name)}
+                  />
+                );
+              }}
+            />
+          </div>
+        </div>
+        {ingredientsArray.length > 0 && <GetRecipesButton />}
+      </div>
     </>
   );
 }
